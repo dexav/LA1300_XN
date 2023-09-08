79d03009818c4ecfd1fc1c73adb11fff36d8e330
dexav <110892637+dexav@users.noreply.github.com>
2023-09-08 11:35:34 +0200

    Add files via upload

---
+++ LA1300_RandomNumberGuesser/Program.cs
@@ -0,0 +1,29 @@
+﻿using LA1300_RandomNumberGuesser.RNGAndPoints;
+using LA1300_RandomNumberGuesser.GenerateNumber;
+using LA1300_RandomNumberGuesser.Player;
+
+
+using System;
+
+namespace LA1300_RandomNumberGuesser
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+       
+
+            Console.WriteLine("Geben gib deinen Namen ein:");
+            PlayerCreate player1 = new PlayerCreate(Console.ReadLine());
+
+           
+            GuessNumb guessingGame = new GuessNumb();
+            guessingGame.MakeGuess();
+
+            Console.WriteLine($"Danke fürs Spielen {player1.PlayerName} bis zum nächsten Mal.");
+            
+            
+        }
+    }
+}
